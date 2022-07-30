341c7fe6b53e825767515201c7b5a6f3c2fa18fc
Jaifran Hernández <jaifranjose@gmail.com>
2022-07-30 16:10:35 -0500

    Refactoring the sidebar component to improve the architecture of the project

--- pages/dashboard/canales.js
+++ pages/dashboard/canales.js
@@ -2,10 +2,7 @@ import React, { useState } from "react"
 import {
 	Flex,
 	Heading,
-	Avatar,
-	AvatarGroup,
 	Text,
-	Icon,
 	IconButton,
 	Table,
 	Thead,
@@ -14,37 +11,21 @@ import {
 	Th,
 	Td,
 	Divider,
-	Link,
 	Box,
 	Button,
 	ButtonGroup,
-	Input,
-	InputGroup,
-	InputLeftElement,
-	
 } from "@chakra-ui/react"
 import {
-	FiHome,
 	FiChevronDown,
 	FiChevronUp,
-	FiPlus,
-	FiBookOpen,
-	FiLayers,
-	FiUsers,
-	FiUser,
-	FiFolderPlus,
-	FiXOctagon,
-	FiCheckSquare,
-	FiSquare,
-	FiGrid,
 } from "react-icons/fi"
 
 import KeysModal from '../../components/modalsFolder/KeysModal'
-import MyOperations from '../../components/MyOperations'
+import MyOperations from '../../components/modalsFolder/OperationsModal'
+import SideBar from "../../components/layout/sideBar"
 
 export default function Canales() {
 	const [display, changeDisplay] = useState("hide")
-	const [value, changeValue] = useState(1)
 	return (
 		<Flex
 			h={[null, null, "100vh"]}
@@ -62,153 +43,7 @@ export default function Canales() {
 					flexDir='column'
 					h={[null, null, "100vh"]}
 					justifyContent='space-between'>
-					<Flex flexDir='column' as='nav'>
-						<Heading
-							mt={50}
-							mb={[25, 50, 100]}
-							fontSize={["4xl", "4xl", "2xl", "3xl", "4xl"]}
-							alignSelf='center'
-							letterSpacing='tight'>
-							Logo
-						</Heading>
-						<Flex
-							flexDir={[
-								"row",
-								"row",
-								"column",
-								"column",
-								"column",
-							]}
-							align={[
-								"center",
-								"center",
-								"center",
-								"flex-start",
-								"flex-start",
-							]}
-							wrap={[
-								"wrap",
-								"wrap",
-								"nowrap",
-								"nowrap",
-								"nowrap",
-							]}
-							justifyContent='center'>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon as={FiHome} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/home'>
-									<Text>Home</Text>
-								</Link>
-							</Flex>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/canales'>
-									<Icon
-										as={FiLayers}
-										fontSize='2xl'
-										className='active-icon'
-									/>
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/canales'>
-									<Text className='active'>Canales</Text>
-								</Link>
-							</Flex>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon as={FiBookOpen} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/bitacora'>
-									<Text>Bitacora</Text>
-								</Link>
-							</Flex>
-							{/* <Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon as={FiUsers} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/users'>
-									<Text>Usuarios</Text>
-								</Link>
-							</Flex> */}
-						</Flex>
-					</Flex>
-					<Flex flexDir='column' alignItems='center' mb={10} mt={5}>
-						<Icon as={FiUser} fontSize='4xl' />
-						<Text textAlign='center'>Alan Turing</Text>
-					</Flex>
+					<SideBar />
 				</Flex>
 			</Flex>
 
