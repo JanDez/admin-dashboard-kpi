2410186a08e50a37ef3f6317199cbc0efa39573b
Jaifran Hernández <jaifranjose@gmail.com>
2022-07-31 00:56:31 -0500

    Code review/improvement

--- pages/dashboard/users.js
+++ pages/dashboard/users.js
@@ -2,19 +2,13 @@ import React, { useState } from "react"
 import {
 	Flex,
 	Heading,
-	Avatar,
-	AvatarGroup,
 	Text,
-	Icon,
-	IconButton,
 	Table,
 	Thead,
 	Tbody,
 	Tr,
 	Th,
 	Td,
-	Divider,
-	Link,
 	Box,
 	Button,
 	Input,
@@ -23,23 +17,11 @@ import {
 	InputLeftElement,
 } from "@chakra-ui/react"
 import {
-	FiHome,
-	FiCalendar,
-	FiChevronDown,
-	FiChevronUp,
-	FiPlus,
 	FiSearch,
-	FiBookOpen,
-	FiKey,
-	FiLayers,
-	FiUsers,
-	FiUser,
 } from "react-icons/fi"
 
-import { VscCalendar } from "react-icons/vsc"
+import SideBar from "../../components/layout/sideBar"
 export default function Bitacora() {
-	const [display, changeDisplay] = useState("hide")
-	const [value, changeValue] = useState(1)
 	return (
 		<Flex
 			h={[null, null, "100vh"]}
@@ -53,158 +35,7 @@ export default function Bitacora() {
 				alignItems='center'
 				backgroundColor='#020202'
 				color='#fff'>
-				<Flex
-					flexDir='column'
-					h={[null, null, "100vh"]}
-					justifyContent='space-between'>
-					<Flex flexDir='column' as='nav'>
-						<Heading
-							mt={50}
-							mb={[25, 50, 100]}
-							fontSize={["4xl", "4xl", "2xl", "3xl", "4xl"]}
-							alignSelf='center'
-							letterSpacing='tight'>
-							Logo
-						</Heading>
-						<Flex
-							flexDir={[
-								"row",
-								"row",
-								"column",
-								"column",
-								"column",
-							]}
-							align={[
-								"center",
-								"center",
-								"center",
-								"flex-start",
-								"flex-start",
-							]}
-							wrap={[
-								"wrap",
-								"wrap",
-								"nowrap",
-								"nowrap",
-								"nowrap",
-							]}
-							justifyContent='center'>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon
-										as={FiHome}
-										fontSize='2xl'
-										className='active-icon'
-									/>
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard'>
-									<Text className='active'>Home</Text>
-								</Link>
-							</Flex>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/canales'>
-									<Icon as={FiLayers} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/canales'>
-									<Text>Canales</Text>
-								</Link>
-							</Flex>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon as={FiBookOpen} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/bitacora'>
-									<Text>Bitacora</Text>
-								</Link>
-							</Flex>
-							<Flex
-								className='sidebar-items'
-								mr={[2, 6, 0, 0, 0]}>
-								<Link
-									display={[
-										"none",
-										"none",
-										"flex",
-										"flex",
-										"flex",
-									]}>
-									<Icon as={FiUsers} fontSize='2xl' />
-								</Link>
-								<Link
-									_hover={{ textDecor: "none" }}
-									display={[
-										"flex",
-										"flex",
-										"none",
-										"flex",
-										"flex",
-									]}
-									href='/dashboard/users'>
-									<Text>Usuarios</Text>
-								</Link>
-							</Flex>
-						</Flex>
-					</Flex>
-					<Flex flexDir='column' alignItems='center' mb={10} mt={5}>
-						<Icon as={FiUser} fontSize='4xl' />
-						<Text textAlign='center'>Alan Turing</Text>
-					</Flex>
-				</Flex>
+				<SideBar />
 			</Flex>
 
 			{/* Column 2 */}
@@ -368,11 +199,6 @@ export default function Bitacora() {
 													letterSpacing='tight'>
 													Info
 												</Heading>
-												{/* <Text
-													fontSize='sm'
-													color='gray'>
-													Timestamp
-												</Text> */}
 											</Flex>
 										</Flex>
 									</Td>
